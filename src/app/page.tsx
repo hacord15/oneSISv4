8de54bd420be03f42f4eb77a08cd6dc286320c95
Abhineet Pal <111232688+hacord15@users.noreply.github.com>
2026-07-06 15:09:17 +0530

    add 2 sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,8 +27,8 @@ export default function Home() {
         <PropertyManagement />
         <FourOutcomes />
         <Accountability />
-        {/* <WhereWeOperate />
-        <TechPlatforms /> */}
+        <WhereWeOperate />
+        <TechPlatforms />
         <CTA />
       </main>
       <Footer />
